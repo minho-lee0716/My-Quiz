d67bfcfe9b85e26a5b8287cd9c5f37ffe37c04a4
Min-Ho Lee <minho.lee0716@gmail.com>
2020-11-10 23:58:50 +0900

    RandomQuiz View까지 작성

--- quiz/views.py
+++ quiz/views.py
@@ -3,6 +3,15 @@ from rest_framework.decorators import api_view
 from .models                   import Quiz
 from .serializers              import QuizSerializer
 
+import random
+
 @api_view(['GET'])
 def helloAPI(request):
     return Response("hello world!")
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs  = Quiz.objects.all()
+    randomQuizs = random.sample(list(totalQuiz), id)
+    serializer = QuizSerializer(randomQuizs, many=True)
+    return Response(serializer.data)
